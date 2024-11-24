cdf1bdd88e0626ef53e09d47a704922c74c66e6b
anwesana kanungo <anwesanakanungo@gmail.com>
2024-11-24 20:06:46 +0530

    readingobjectExample

---
+++ src/main/java/com/java/ui/uienum/WaitStrategy.java
@@ -0,0 +1,8 @@
+package com.java.ui.uienum;
+
+public enum WaitStrategy {
+    CLICKABLE,
+    PRESENCE,
+    VISIBLE,
+    NONE
+}
--- src/test/java/com/test/api/testcases/TestCreateBooking.java
+++ src/test/java/com/test/api/testcases/TestCreateBooking.java
@@ -1,12 +1,15 @@
 package com.test.api.testcases;
 
 
+import com.test.api.utils.JsonReader;
 import org.testng.annotations.Test;
 
 public class TestCreateBooking {
     @Test
-    public void updateBookingByReadingJsonFile() {
-
+    public void CreateBookingByReadingJsonFile() {
+        String s = JsonReader.readJsonFileGetAsString("src/main/resources/jsonfile/Resonse.json");
+        String t=s.replace("fname","new455555555555");
+        System.out.println(t);
     }
 
 }
